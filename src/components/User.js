8fcaa2de490eb16643ef24d8fa5403f32e007ace
Neelam618 <neelamchavan410@gmail.com>
2021-05-17 14:28:09 +0530

    added withRouter

--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,13 @@
 import React from 'react'
-// import {} from 'react-router-dom'
+import {withRouter} from 'react-router-dom'
 
-export default function User() {
+function User(props) {
+    console.warn(props)
     return (
         <div>
             <h1>User Component</h1>
         </div>
     )
 }
+
+export default withRouter(User)
